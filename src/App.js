0ea8cd3b1cf156eeeaad266d37239c2e14823c44
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-10-04 12:43:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+  const [hotels, setHotels] = useState(data);
+  const [showText, setShowText] = useState(false);
+
+  const removeHotel = (id) => {
+    let newHotels = hotels.filter((hotel) => hotel.id !== id);
+    setHotels(newHotels);
+  }
+
+  const showTextClick = (hotel) => {
+    hotel.showMore = !hotel.showMore
+    setShowText(!showText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>NYC TOP {hotels.length} HOTELS</h1>
+      </div>
+      {hotels.map((hotel => {
+        const { id, hotelName, description, image, source, showMore} = hotel;
+        return(
+          <div key={id}>
+            <div className="container">
+              <h2> {id} - {hotelName}</h2>
+            </div>
+            <div className="container">
+              <p> {showMore ? description : description.substring(0, 220) + "...."}
+              <button onClick={() => showTextClick(hotel)}>{showMore ? "Show less" : "Show more"}</button> </p>
+            </div>
+            <div className="container">
+              <img src={image} alt="NYC" width="500px"/>
+            </div>
+            <div className="container">
+              <p>{source}</p>
+            </div>
+            <div className="container">
+              <button className="btn" onClick={() => removeHotel(id)}>remove</button>
+            </div>
+          </div>
+        )
+      }))}
     </div>
   );
 }
